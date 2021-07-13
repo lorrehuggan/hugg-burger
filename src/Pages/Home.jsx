dcab4a84f46dca3728c94ca6b3c4c847ccd20dbd
Lorre Huggan <lorre.huggan@protonmail.com>
2021-07-13 06:43:07 +0100

    home page setup and complete just need to connect form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,9 @@ import "./Home.scss";
 import BigReady from "../img/Big-Ready.png";
 import BaconDouble from "../img/Bacon-Double.png";
 import TexasStacker from "../img/Texas-Stacker.png";
+import Deliveroo from "../img/Deliveroo.png";
+import JustEat from "../img/Just-Eat.png";
+import UberEats from "../img/Uber-Eats.png";
 
 function Home() {
   return (
@@ -14,15 +17,35 @@ function Home() {
       <div className="home">
         <div className="home--container">
           <div className="home--header">
-            <h1>Ready For A Burger?</h1>
+            <h1>Ready for a better burger?</h1>
           </div>
           <div className="home--img">
             <img src={BigReady} alt="bigready-burger" />
             <img src={BaconDouble} alt="bacondouble-burger" />
             <img src={TexasStacker} alt="texas-burger" />
           </div>
+          <div className="home--sticker__container">
+            <div className="home--sticker">
+              <div className="sticker">
+                <img src={Deliveroo} alt="deliveroo" />
+              </div>
+              <div className="sticker">
+                <img src={JustEat} alt="justeat" />
+              </div>
+              <div className="sticker">
+                <img src={UberEats} alt="ubereats" />
+              </div>
+            </div>
+          </div>
           <div className="home--img__img-bg"></div>
         </div>
+      </div>
+      <div className="home--bottom__msg">
+        <p>
+          Ready Burger brings you classic flavours at everyday prices. But,
+          what's the difference?
+        </p>
+        <p style={{ fontWeight: "900" }}>Our menu is 100% plant-based.</p>
       </div>
       <Footer />
     </React.Fragment>
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import Navigation from '../Components/Navigation'
-import './Menu.scss'
-import MenuCard from '../Components/MenuCard'
-import { menuData } from '../Data/data'
+import React from "react";
+import Navigation from "../Components/Navigation";
+import "./Menu.scss";
+import MenuCard from "../Components/MenuCard";
+
+import Footer from "../Components/Footer";
+import { menuData } from "../Data/data";
 
 function Menu() {
-    return (
-        <div className='menu'>
-            <Navigation/>
-            <div className="menu--container">
-                <div className="menu--cards">
-                    {menuData.map((card)=>{
-                        return (<MenuCard  img={card.img} type={card.class} name={card.name} price={card.price}/>)
-                    })}
-                </div>
-            </div>
+  return (
+    <React.Fragment>
+      <Navigation />
+      <div className="menu">
+        <div className="menu--container">
+          <div className="menu--cards">
+            {menuData.map((card) => {
+              return (
+                <MenuCard
+                  img={card.img}
+                  type={card.class}
+                  name={card.name}
+                  price={card.price}
+                />
+              );
+            })}
+          </div>
         </div>
-    )
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
-export default Menu
+export default Menu;
